b76a502e8e0f5d8f302ca135aefd43adef68557f
Gokhan <129348617+gokhansen19@users.noreply.github.com>
2026-01-11 21:25:10 +0300

    Convert script to a Discord bot with commands
    Refactor to create a Discord bot with commands.

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-meme_dict= {
-            "CRINGE": "Garip ya da utandırıcı bir şey",
-            "LOL": "Komik bir şeye verilen cevap",
-            "ROFL": "ROFL bir şakaya karşılıktır, LOL gibidir",
-            "SHEESH" : "onaylamamak",
-            "CREEPY" : "korkunç",
-            "AGGRO" : "agresifleşmek/sinirlenmek"
-            }
+import discord
+from discord.ext import commands
 
-word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+intents = discord.Intents.default()
+intents.message_content = True
 
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print("Henüz bu kelimeye sahip değiliz... Ama üzerinde çalışıyoruz!")
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("GİZLİ TOKEN BURAYA")
